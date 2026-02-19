c31b6859f9f9b982ec41936c96a762941bef19f8
Jershw43 <95701884+jershw43@users.noreply.github.com>
2026-02-19 11:17:46 -0500

    Added Scanner outline files

--- Scanner/file_util.h
+++ Scanner/file_util.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 
 /* Constants */
+#define PRINT_RED     "\x1b[31m"
+#define PRINT_YELLOW  "\x1b[33m"
+#define PRINT_GREEN   "\x1b[32m"
+#define PRINT_BLUE    "\x1b[34m"
+#define PRINT_CYAN    "\x1b[36m"
+#define PRINT_ITALIC  "\x1b[3m"
+#define PRINT_RESET   "\x1b[0m"
+
 #define MAX_FILENAME 256
 #define IN  ".IN"
 #define OUT ".OUT"
@@ -19,14 +27,19 @@ extern FILE *global_input_file;
 extern FILE *global_output_file;
 extern FILE *global_listing_file;
 
+extern int global_input_opened;
+extern int global_output_opened;
+extern int global_listing_opened;
+
 /* ---------- FUNCTION PROTOTYPES ---------- */
 void open_input_file(int argc, char *argv[]);
-void open_output_file(void);
+void open_output_file(int argc, char *argv[]);
 void open_listing_file(void);
 void create_temp_files(void);
-void validate_names(void);
+int validate_names(void);
 void generate_extensions(void);
 void backup_output(void);
 void file_close(void);
+void file_status(char* name, char* filename, FILE *file, int is_opened, char* color);
 
 #endif
